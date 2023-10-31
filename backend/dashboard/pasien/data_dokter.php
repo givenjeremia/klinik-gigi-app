2a906708044c4496e0deaeb4d5a56fae61988407
givenjeremia <jeremiagiven@gmail.com>
2023-10-31 21:46:14 +0700

    fix dashboard data dokter today

--- backend/dashboard/pasien/data_dokter.php
+++ backend/dashboard/pasien/data_dokter.php
@@ -2,16 +2,15 @@
 require_once( '../../config.php' );
 session_start();
 $day_now = date( 'l' );
-$id_user = $_SESSION[ 'auth' ][ 'id' ];
-$stmt_pasien = $mysqli->prepare( 'SELECT * FROM `data_pasien` WHERE `user_id` =?' );
-$stmt_pasien->bind_param( 'i', $id_user );
-$stmt_pasien->execute();
-$result_pasien = $stmt_pasien->get_result();
-$data_pasien = $result_pasien->fetch_assoc();
-$id_pasien = $data_pasien[ 'id' ];
-$sql = "SELECT dd.id as id_dokter, dd.nama as nama_dokter, jd.id as id_jadwal , jd.jam as jam, jd.hari as hari, jd.kuota_pasien as kuota FROM `data_dokter` dd INNER JOIN `jadwal_dokter` jd ON dd.id = jd.data_dokter_id WHERE NOT EXISTS (
-  SELECT $id_pasien FROM `reservasi_kllinik` rk WHERE rk.jadwal_dokter_id = jd.id AND rk.data_pasien_id_pasien = $id_pasien OR rk.status = 'rejected'
-  ) AND jd.hari = '$day_now'";
+// $id_user = $_SESSION[ 'auth' ][ 'id' ];
+// $stmt_pasien = $mysqli->prepare( 'SELECT * FROM `data_pasien` WHERE `user_id` =?' );
+// $stmt_pasien->bind_param( 'i', $id_user );
+// $stmt_pasien->execute();
+// $result_pasien = $stmt_pasien->get_result();
+// $data_pasien = $result_pasien->fetch_assoc();
+// $id_pasien = $data_pasien[ 'id' ];
+
+$sql = "SELECT dd.id as id_dokter, dd.nama as nama_dokter, jd.id as id_jadwal , jd.jam as jam, jd.hari as hari, jd.kuota_pasien as kuota FROM `data_dokter` dd INNER JOIN `jadwal_dokter` jd ON dd.id = jd.data_dokter_id WHERE jd.hari = '$day_now'";
 $result = $mysqli->query( $sql );
 $datas = [];
 if ( $result->num_rows > 0 ) {
