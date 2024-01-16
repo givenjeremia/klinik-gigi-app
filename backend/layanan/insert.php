b9dac67729378ab704e62182f58cc2adfb4b9ed6
Given Jeremia <jeremiagiven@gmail.com>
2024-01-16 11:32:20 +0700

    fixed layanan

--- backend/layanan/insert.php
+++ backend/layanan/insert.php
@@ -2,9 +2,19 @@
 require_once( '../config.php' );
 try {
     $harga = str_replace( '.', '', $_POST[ 'harga' ] );
-    $stmt = $mysqli->prepare( 'INSERT INTO `layanan`(`nama`, `jenis`, `harga`,`spesialis_id`) VALUES (?,?,?,?)' );
-    $stmt->bind_param( 'ssdi', $_POST[ 'nama' ], $_POST[ 'jenis' ], $harga,$_POST[ 'spesialis' ] );
-    $stmt->execute();
+    // print_r($_POST[ 'spesialis' ]);
+
+    if ( $_POST[ 'spesialis' ] == 0 ) {
+        $stmt = $mysqli->prepare( 'INSERT INTO `layanan`(`nama`, `jenis`, `harga`,`spesialis_id`) VALUES (?,?,?)' );
+        $stmt->bind_param( 'ssd', $_POST[ 'nama' ], $_POST[ 'jenis' ], $harga);
+        $stmt->execute();
+    }
+    else{
+        $stmt = $mysqli->prepare( 'INSERT INTO `layanan`(`nama`, `jenis`, `harga`,`spesialis_id`) VALUES (?,?,?,?)' );
+        $stmt->bind_param( 'ssdi', $_POST[ 'nama' ], $_POST[ 'jenis' ], $harga,  $_POST[ 'spesialis' ] );
+        $stmt->execute();
+        
+    }
     $jumlah_yang_dieksekusi = $stmt->affected_rows;
     if ( $jumlah_yang_dieksekusi > 0 ) {
         echo json_encode( [ 'status'=>'success', 'nama'=>$_POST[ 'nama' ] ] );
